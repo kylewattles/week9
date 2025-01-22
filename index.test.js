d090161fec38a438aa81c16ce8a1b59ccd2bfcde
kyle wattles <wattles.kyle66@Gmail.com>
2025-01-22 10:00:33 -0800

    adding git to week9 project folder

---
+++ index.test.js
@@ -0,0 +1,13 @@
+const expect = chai.expect;
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            expect([1, 2, 3].indexOf(4)).to.equal(-1);
+        });
+        it('should equal 5', function(){
+           
+            expect(2+"3").to.equal("23")
+        })
+    });
+});
